dc9b067972e62b43a388dc6a8cf132cb14ced0a8
cipepser <respepic@gmail.com>
2018-03-15 23:05:18 +0900

    finish hello_world

---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/respepic/Documents/sand/rust/sample/hello_world/target/debug/hello_world: /Users/respepic/Documents/sand/rust/sample/hello_world/src/main.rs
---
+++ hello_world/target/release/hello_world.d
@@ -0,0 +1,1 @@
+/Users/respepic/Documents/sand/rust/sample/hello_world/target/release/hello_world: /Users/respepic/Documents/sand/rust/sample/hello_world/src/main.rs
