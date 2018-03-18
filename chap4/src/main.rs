23ebcc31f71c396eb4623a08bac8066fe1c3b1f8
cipepser <respepic@gmail.com>
2018-03-18 10:45:39 +0900

    finish 4.7

--- chap4/src/main.rs
+++ chap4/src/main.rs
@@ -26,11 +26,23 @@ fn main() {
     // }
     
     // loop label
-    'outer: for x in 0..10 {
-        'inner: for y in 0..10 {
-            if x % 2 == 0 {continue 'outer; }
-            if y % 2 == 0 {continue 'inner; }
-            println!("x: {}, y: {}", x, y);
-        }
-    }
+    // 'outer: for x in 0..10 {
+    //     'inner: for y in 0..10 {
+    //         if x % 2 == 0 {continue 'outer; }
+    //         if y % 2 == 0 {continue 'inner; }
+    //         println!("x: {}, y: {}", x, y);
+    //     }
+    // }
+    
+    // ownership
+    // VecはCopy traitを実装していないのでuse of move value: `v`でエラー
+    // let v = vec![1, 2, 3];
+    // let v2 = v;
+    // println!("v[0] is: {}", v[0]);
+    // println!("v2[0] is: {}", v2[0]);
+
+    // i32はCopy traitを実装しているので怒られない
+    let x  = 1;
+    let x2 = x;
+    println!("{}", x2);
 }
