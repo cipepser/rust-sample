4cb7ad395d6eb861973897513ee575611185f4dc
cipepser <respepic@gmail.com>
2018-03-18 10:29:23 +0900

    finish 4.6

--- chap4/src/main.rs
+++ chap4/src/main.rs
@@ -8,14 +8,29 @@ fn main() {
     // println!("{}", b[2]);
     
     // if
-    let x = 5;
-    if x == 5 {
-        println!("x is 5");
-    } else {
-        println!("x is not 5");
+    // let x = 5;
+    // if x == 5 {
+    //     println!("x is 5");
+    // } else {
+    //     println!("x is not 5");
+    // }
+    //
+    // let y = if x == 5 { 10 } else { 15 };
+    //
+    // println!("{}", y);
+    
+    // enum
+    // go の for i, v := rangeみたいな感じ
+    // for (i, j) in (5..10).enumerate() {
+    //     println!("i = {} and j = {}", i, j);
+    // }
+    
+    // loop label
+    'outer: for x in 0..10 {
+        'inner: for y in 0..10 {
+            if x % 2 == 0 {continue 'outer; }
+            if y % 2 == 0 {continue 'inner; }
+            println!("x: {}, y: {}", x, y);
+        }
     }
-    
-    let y = if x == 5 { 10 } else { 15 };
-    
-    println!("{}", y);
 }
