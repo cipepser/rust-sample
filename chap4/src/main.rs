fe149904b8f262b588cb2a33f464dad8915813b9
cipepser <respepic@gmail.com>
2018-03-23 01:23:44 +0900

    finish 4.19

--- chap4/src/main.rs
+++ chap4/src/main.rs
@@ -8,16 +8,49 @@
 // }
 
 // call method
+// struct Circle {
+//     x: f64,
+//     y: f64,
+//     radius: f64,
+// }
+//
+// impl Circle {
+//     fn area(&self) -> f64 {
+//         std::f64::consts::PI * (self.radius * self.radius)
+//     }
+// }
+
+// trait
+trait HasArea {
+    fn area(&self) -> f64;
+}
+
 struct Circle {
     x: f64,
     y: f64,
     radius: f64,
 }
 
-impl Circle {
+impl HasArea for Circle {
     fn area(&self) -> f64 {
         std::f64::consts::PI * (self.radius * self.radius)
     }
+}
+
+struct Square {
+    x: f64,
+    y: f64,
+    side: f64,
+}
+
+impl HasArea for Square {
+    fn area(&self) -> f64 {
+        self.side * self.side
+    }
+}
+
+fn print_area<T: HasArea>(shape: T) {
+    println!("This shape has an area of {}", shape.area())
 }
 
 fn main() {
@@ -210,7 +243,21 @@ fn main() {
     //     OptionalInt::Missing => println!("No such luck."),
     // }
 
-    let c = Circle { x: 0.0, y: 0.0, radius: 2.0 };
-    println!("{}", c.area());
+    // let c = Circle { x: 0.0, y: 0.0, radius: 2.0 };
+    // println!("{}", c.area());
+    
+    let c = Circle {
+        x: 0.0f64,
+        y: 0.0f64,
+        radius: 1.0f64,
+    };
 
+    let s = Square {
+        x: 0.0f64,
+        y: 0.0f64,
+        side: 1.0f64,
+    };
+
+    print_area(c);
+    print_area(s);
 }
--- chap4/target/debug/chap4-ac0fba3c8cd67740.d
+++ chap4/target/debug/chap4-ac0fba3c8cd67740.d
@@ -1,1 +1,1 @@
-/Users/respepic/Documents/sand/rust/sample/chap4/target/debug/chap4-ac0fba3c8cd67740.dSYM: /Users/respepic/Documents/sand/rust/sample/chap4/src/lib.rs /Users/respepic/Documents/sand/rust/sample/chap4/src/main.rs
+/Users/respepic/Documents/sand/rust/sample/chap4/target/debug/chap4-ac0fba3c8cd67740.dSYM: /Users/respepic/Documents/sand/rust/sample/chap4/src/main.rs /Users/respepic/Documents/sand/rust/sample/chap4/src/lib.rs
