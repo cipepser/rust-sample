17f3a4928eaad6ca68b34809b6416673418228ad
cipepser <respepic@gmail.com>
2018-04-28 16:34:46 +0900

    write a channel

--- concurrent/src/main.rs
+++ concurrent/src/main.rs
@@ -8,20 +8,47 @@
 //     println!("{}", handle.join().unwrap());
 // }
 
-use std::thread;
+// 安全なshared mutable state
+// use std::thread;
+// use std::sync::{Arc, Mutex};
+// use std::time::Duration;
+//
+// fn main() {
+//     let data = Arc::new(Mutex::new(vec![1, 2, 3]));
+//
+//     for i in 0..3 {
+//         let data = data.clone();
+//         thread::spawn(move || {
+//             let mut data = data.lock().unwrap();
+//             data[i] += 1;
+//         });
+//     }
+//
+//     thread::sleep(Duration::from_millis(50));
+// }
+
+// channel
 use std::sync::{Arc, Mutex};
-use std::time::Duration;
+use std::thread;
+use std::sync::mpsc;
 
 fn main() {
-    let data = Arc::new(Mutex::new(vec![1, 2, 3]));
+    let data = Arc::new(Mutex::new(0));
     
-    for i in 0..3 {
-        let data = data.clone();
+    let (tx, rx) = mpsc::channel();
+    
+    for _ in 0..10 {
+        let (data, tx) = (data.clone(), tx.clone());
+        
         thread::spawn(move || {
             let mut data = data.lock().unwrap();
-            data[i] += 1;
+            *data += 1;
+            
+            tx.send(()).unwrap();
         });
     }
     
-    thread::sleep(Duration::from_millis(50));
+    for _ in 0..10 {
+        rx.recv().unwrap();
+    }
 }
