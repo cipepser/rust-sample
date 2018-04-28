8e2ee8ea402715587e4acbf91299ceeaba2ae5bb
cipepser <respepic@gmail.com>
2018-04-28 16:38:10 +0900

    add a example of channel

--- concurrent/src/main.rs
+++ concurrent/src/main.rs
@@ -28,27 +28,81 @@
 // }
 
 // channel
-use std::sync::{Arc, Mutex};
+// use std::sync::{Arc, Mutex};
+// use std::thread;
+// use std::sync::mpsc;
+//
+// fn main() {
+//     let data = Arc::new(Mutex::new(0));
+//
+//     let (tx, rx) = mpsc::channel();
+//
+//     for _ in 0..10 {
+//         let (data, tx) = (data.clone(), tx.clone());
+//
+//         thread::spawn(move || {
+//             let mut data = data.lock().unwrap();
+//             *data += 1;
+//
+//             tx.send(()).unwrap();
+//         });
+//     }
+//
+//     for _ in 0..10 {
+//         rx.recv().unwrap();
+//     }
+// }
+
+
 use std::thread;
 use std::sync::mpsc;
 
 fn main() {
-    let data = Arc::new(Mutex::new(0));
-    
     let (tx, rx) = mpsc::channel();
     
-    for _ in 0..10 {
-        let (data, tx) = (data.clone(), tx.clone());
+    for i in 0..10 {
+        let tx = tx.clone();
         
         thread::spawn(move || {
-            let mut data = data.lock().unwrap();
-            *data += 1;
+            let answer = i * i;
             
-            tx.send(()).unwrap();
+            tx.send(answer).unwrap();
         });
     }
     
     for _ in 0..10 {
-        rx.recv().unwrap();
+        println!("{}", rx.recv().unwrap());
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
