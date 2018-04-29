b26cc2c98efcfbc6fa529f74673ef372a7fddfc5
cipepser <respepic@gmail.com>
2018-04-29 14:30:22 +0900

    write try! macro

--- error/src/main.rs
+++ error/src/main.rs
@@ -53,23 +53,50 @@
 // }
 
 // 早期リターン
+// use std::fs::File;
+// use std::io::Read;
+// use std::path::Path;
+//
+// fn file_double<P: AsRef<Path>>(file_path: P) -> Result<i32, String> {
+//     let mut file = match File::open(file_path) {
+//         Ok(file) => file,
+//         Err(err) => return Err(err.to_string()),
+//     };
+//     let mut contents = String::new();
+//     if let Err(err) = file.read_to_string(&mut contents) {
+//         return Err(err.to_string());
+//     }
+//     let n: i32 = match contents.trim().parse() {
+//         Ok(n) => n,
+//         Err(err) => return Err(err.to_string()),
+//     };
+//     Ok(2 * n)
+// }
+//
+// fn main() {
+//     match file_double("foobar") {
+//         Ok(n) => println!("{}", n),
+//         Err(err) => println!("Error: {}", err),
+//     }
+// }
+
+// try! マクロ
+macro_rules! try {
+    ($e:expr) => (match $e {
+        Ok(val) => val,
+        Err(err) => return Err(err),
+    });
+}
+
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
 fn file_double<P: AsRef<Path>>(file_path: P) -> Result<i32, String> {
-    let mut file = match File::open(file_path) {
-        Ok(file) => file,
-        Err(err) => return Err(err.to_string()),
-    };
+    let mut file = try!(File::open(file_path).map_err(|e| e.to_string()));
     let mut contents = String::new();
-    if let Err(err) = file.read_to_string(&mut contents) {
-        return Err(err.to_string());
-    }
-    let n: i32 = match contents.trim().parse() {
-        Ok(n) => n,
-        Err(err) => return Err(err.to_string()),
-    };
+    try!(file.read_to_string(&mut contents).map_err(|e| e.to_string()));
+    let n = try!(contents.trim().parse::<i32>().map_err(|e| e.to_string()));
     Ok(2 * n)
 }
 
@@ -79,11 +106,6 @@ fn main() {
         Err(err) => println!("Error: {}", err),
     }
 }
-
-
-
-
-
 
 
 
